d6833551c78e950bcaee5558d97110d31e8d0bc3
Catzu <that-@hotmail.nl>
2020-09-07 12:57:27 +0200

    remodeling 000WEBHOST website

---
+++ PHPY1/PHPCL/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+    <style>
+
+    </style>
+    <body>
+        <header>
+            <h3>Welcome to That's website</h3>
+        </header>
+        <section>
+            <h3>PHP Year 1</h3>
+        </section>
+        <section>
+            <h3>PHP chapter list</h3>
+        </section>
+        <section>
+            <button type="button"><a href="PHPCL/C1/index.php">Chapter 01</a></button>
+        </section>
+        <br>
+        <section>
+            <button type="button"><a href="PHPCL/C2/index.php">Chapter 02</a></button>
+        </section>
+        <br>
+        <section>
+            <button type="button"><a href="PHPCL/C3/index.php">Chapter 03</a></button>
+        </section>
+        <br>
+        <section>
+            <button type="button"><a href="PHPCL/C4/index.php">Chapter 04</a></button>
+        </section>
+        <br>
+        <section>
+            <button type="button"><a href="PHPCL/C5/index.php">Chapter 05</a></button>
+        </section>
+        <br>
+        <section>
+            <button type="button"><a href="PHPCL/C6/index.php">Chapter 06</a></button>
+        </section>
+        <br>
+        <section>
+            <button type="button"><a href="PHPCL/C7/index.php">Chapter 07</a></button>
+        </section>
+        <br>
+    </body>
+</html>
--- index.php
+++ index.php
@@ -8,34 +8,14 @@
             <h3>Welcome to That's website</h3>
         </header>
         <section>
-            <h3>PHP chapter list</h3>
+            <h3>PHP</h3>
         </section>
         <section>
-            <button type="button"><a href="C1/index.php">Chapter 01</a></button>
-        </section>
-        <br>
-        <section>
-            <button type="button"><a href="C2/index.php">Chapter 02</a></button>
+            <button type="button"><a href="PHPY1/index.php">PHP Y1</a></button>
         </section>
         <br>
         <section>
-            <button type="button"><a href="C3/index.php">Chapter 03</a></button>
-        </section>
-        <br>
-        <section>
-            <button type="button"><a href="C4/index.php">Chapter 04</a></button>
-        </section>
-        <br>
-        <section>
-            <button type="button"><a href="C5/index.php">Chapter 05</a></button>
-        </section>
-        <br>
-        <section>
-            <button type="button"><a href="C6/index.php">Chapter 06</a></button>
-        </section>
-        <br>
-        <section>
-            <button type="button"><a href="C7/index.php">Chapter 07</a></button>
+            <button type="button"><a href="">PHP Y2</a></button>
         </section>
         <br>
     </body>
